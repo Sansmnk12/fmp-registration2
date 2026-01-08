bfdbbed451864168db671e65d21b6424fceb59ca
Sansmnk12 <san.smnk@protonmail.com>
2026-01-08 07:25:45 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 // Paste your Supabase project URL and anon key here, then deploy.
-window.SUPABASE_URL = "PASTE_SUPABASE_URL";
-window.SUPABASE_ANON_KEY = "PASTE_SUPABASE_ANON_KEY";
+window.SUPABASE_URL = "https://jyztjdemrcgzczssndqg.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp5enRqZGVtcmNnemN6c3NuZHFnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njc4Mjk2NDMsImV4cCI6MjA4MzQwNTY0M30.y1FZCFPhvAi-sUUM_MZRFHt4nbJC0Te_92RxZabL4_8";
